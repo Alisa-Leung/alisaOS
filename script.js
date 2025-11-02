2337368e6fdc3488c63907b5cb47dbb6fc9c52cf
Alisa Leung <alisal.singyee@gmail.com>
2025-11-01 19:54:08 -0700

    figuring out time

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function updateTime(){
-        const time = document.getElementById("time");
-        var currentTime = new Date().toLocaleString();
-        time.innerHTML = currentTime
-
-    }
-    setInterval(updateTime, 1000);
+    setInterval(function (){
+        document.querySelector("#time").innerHTML = new Date().toLocaleString();
+    }, 1000);
 });
